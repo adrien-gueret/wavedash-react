31040282409fc9590504650f7fbc657deb2f1811
Adrien Guéret <adrien.grt@gmail.com>
2026-05-10 00:05:02 +0200

    feat(achievement,stat): add hooks to handle achievments and stats

---
+++ src/useAchievement.ts
@@ -0,0 +1,46 @@
+import { useCallback, useState, useEffect } from "react";
+
+import { useWavedash } from "./WavedashProvider";
+
+export type UseAchievementReturnValue = [
+  boolean | null,
+  unlock: (storeNow?: boolean) => boolean,
+];
+
+export default function useAchievement(
+  achievementName: string,
+): UseAchievementReturnValue {
+  const { isRunningInWavedash, wavedash } = useWavedash();
+
+  const [isUnlocked, setIsUnlocked] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (!isRunningInWavedash || isUnlocked !== null) {
+      return;
+    }
+
+    wavedash.requestStats().then(() => {
+      const value = wavedash.getAchievement(achievementName);
+      setIsUnlocked(value);
+    });
+  }, [isRunningInWavedash, wavedash, achievementName, isUnlocked]);
+
+  const unlock = useCallback(
+    (storeNow: boolean = true): boolean => {
+      if (!isRunningInWavedash) {
+        return false;
+      }
+
+      const response = wavedash.setAchievement(achievementName, storeNow);
+
+      if (response) {
+        setIsUnlocked(true);
+      }
+
+      return response;
+    },
+    [isRunningInWavedash, wavedash, achievementName],
+  );
+
+  return [isUnlocked, unlock];
+}
---
+++ src/useStat.ts
@@ -0,0 +1,44 @@
+import { useCallback, useState, useEffect } from "react";
+
+import { useWavedash } from "./WavedashProvider";
+
+export type UseStatReturnValue = [
+  number | null,
+  (newValue: number, storeNow?: boolean) => boolean,
+];
+
+export default function useStat(statName: string): UseStatReturnValue {
+  const { isRunningInWavedash, wavedash } = useWavedash();
+
+  const [statValue, setStatValue] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (!isRunningInWavedash || statValue !== null) {
+      return;
+    }
+
+    wavedash.requestStats().then(() => {
+      const value = wavedash.getStat(statName);
+      setStatValue(value);
+    });
+  }, [isRunningInWavedash, wavedash, statName, statValue]);
+
+  const set = useCallback(
+    (newValue: number, storeNow: boolean = true): boolean => {
+      if (!isRunningInWavedash) {
+        return false;
+      }
+
+      const response = wavedash.setStat(statName, newValue, storeNow);
+
+      if (response) {
+        setStatValue(newValue);
+      }
+
+      return response;
+    },
+    [isRunningInWavedash, wavedash, statName],
+  );
+
+  return [statValue, set];
+}
