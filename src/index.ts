2d0aba4c369bfc0ab668ebb4c576e035f50cbf48
Adrien Guéret <adrien.grt@gmail.com>
2026-05-09 23:32:37 +0200

    feat(leaderboards): handle leaderboards

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./UserAvatar";
 export * from "./useCurrentUser";
+export * from "./useLeaderboard";
 export * from "./WavedashProvider";
---
+++ src/useLeaderboard.ts
@@ -0,0 +1,286 @@
+import { useState, useEffect, useCallback } from "react";
+import type {
+  Leaderboard,
+  LeaderboardSortOrder,
+  LeaderboardDisplayType,
+  Id,
+  LeaderboardEntries,
+} from "@wvdsh/sdk-js";
+
+import { useWavedash } from "./WavedashProvider";
+
+export type UseLeaderBoardReturnValue = {
+  isLoading: boolean;
+  leaderboard: Leaderboard | null;
+  submitScore: (
+    score: number,
+    keepBest?: boolean,
+    ugcId?: Id<"userGeneratedContent">,
+  ) => Promise<number | null>;
+  getEntryCount: () => number;
+  getEntries: (
+    start: number,
+    count: number,
+    friendsOnly?: boolean,
+  ) => Promise<LeaderboardEntries>;
+  getEntriesAroundCurrentUser: (
+    countAhead: number,
+    countBehind: number,
+    friendsOnly?: boolean,
+  ) => Promise<LeaderboardEntries>;
+  getCurrentUserEntries: () => Promise<LeaderboardEntries>;
+};
+
+export type LeaderboardOptions = {
+  sortOrder?: LeaderboardSortOrder;
+  displayType?: LeaderboardDisplayType;
+};
+
+export function useLeaderboard(
+  leaderboardName: string,
+  leaderboardOptions?: LeaderboardOptions,
+): UseLeaderBoardReturnValue {
+  const [leaderboard, setLeaderboard] = useState<Leaderboard | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const { isRunningInWavedash, wavedash } = useWavedash();
+
+  const { sortOrder, displayType } = leaderboardOptions ?? {};
+
+  useEffect(() => {
+    if (!isRunningInWavedash) {
+      setIsLoading(false);
+      return;
+    }
+
+    setIsLoading(true);
+
+    wavedash
+      .getOrCreateLeaderboard(
+        leaderboardName,
+        sortOrder ?? wavedash.LeaderboardSortOrder.DESC,
+        displayType ?? wavedash.LeaderboardDisplayType.NUMERIC,
+      )
+      .then((result) => {
+        if (result.success) {
+          setLeaderboard(result.data);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [isRunningInWavedash, wavedash, leaderboardName, sortOrder, displayType]);
+
+  const leaderboardId = leaderboard?.id;
+
+  const submitScore = useCallback(
+    async (
+      score: number,
+      keepBest: boolean = true,
+      ugcId?: Id<"userGeneratedContent">,
+    ): Promise<number | null> => {
+      if (!isRunningInWavedash || !leaderboardId) {
+        return null;
+      }
+
+      const response = await wavedash.uploadLeaderboardScore(
+        leaderboardId,
+        score,
+        keepBest,
+        ugcId,
+      );
+
+      return response.success ? response.data.globalRank : null;
+    },
+    [isRunningInWavedash, leaderboardId, wavedash],
+  );
+
+  const getEntryCount = useCallback((): number => {
+    if (!isRunningInWavedash || !leaderboardId) {
+      return -1;
+    }
+
+    return wavedash.getLeaderboardEntryCount(leaderboardId);
+  }, [isRunningInWavedash, leaderboardId, wavedash]);
+
+  const getEntries = useCallback(
+    async (
+      start: number,
+      count: number,
+      friendsOnly?: boolean,
+    ): Promise<LeaderboardEntries> => {
+      if (!isRunningInWavedash || !leaderboardId) {
+        return [];
+      }
+
+      const response = await wavedash.listLeaderboardEntries(
+        leaderboardId,
+        start,
+        count,
+        friendsOnly,
+      );
+
+      return response.success ? response.data : [];
+    },
+    [isRunningInWavedash, leaderboardId, wavedash],
+  );
+
+  const getEntriesAroundCurrentUser = useCallback(
+    async (
+      countAhead: number,
+      countBehind: number,
+      friendsOnly?: boolean,
+    ): Promise<LeaderboardEntries> => {
+      if (!isRunningInWavedash || !leaderboardId) {
+        return [];
+      }
+
+      const response = await wavedash.listLeaderboardEntriesAroundUser(
+        leaderboardId,
+        countAhead,
+        countBehind,
+        friendsOnly,
+      );
+
+      return response.success ? response.data : [];
+    },
+    [isRunningInWavedash, leaderboardId, wavedash],
+  );
+
+  const getCurrentUserEntries =
+    useCallback(async (): Promise<LeaderboardEntries> => {
+      if (!isRunningInWavedash || !leaderboardId) {
+        return [];
+      }
+
+      const response = await wavedash.getMyLeaderboardEntries(leaderboardId);
+      return response.success ? response.data : [];
+    }, [isRunningInWavedash, leaderboardId, wavedash]);
+
+  return {
+    isLoading,
+    leaderboard,
+    submitScore,
+    getEntryCount,
+    getEntries,
+    getEntriesAroundCurrentUser,
+    getCurrentUserEntries,
+  };
+}
+
+export type LeaderboardAllEntriesOptions = {
+  start: number;
+  count: number;
+  friendsOnly?: boolean;
+};
+
+export type LeaderboardAroundCurrentUserEntriesOptions = {
+  countAhead: number;
+  countBehind: number;
+  friendsOnly?: boolean;
+};
+
+export function useLeaderboardEntries(
+  leaderboardName: string,
+  leaderboardEntriesOptions: LeaderboardAllEntriesOptions,
+  leaderboardOptions?: LeaderboardOptions,
+): { isLoading: boolean; entries: LeaderboardEntries } {
+  const { isLoading: isLeaderboardLoading, getEntries } = useLeaderboard(
+    leaderboardName,
+    leaderboardOptions,
+  );
+  const [entries, setEntries] = useState<LeaderboardEntries>([]);
+
+  const [areEntriesLoading, setAreEntriesLoading] = useState(true);
+
+  const { start, count, friendsOnly } = leaderboardEntriesOptions;
+
+  useEffect(() => {
+    if (isLeaderboardLoading) {
+      return;
+    }
+
+    setAreEntriesLoading(true);
+
+    getEntries(start, count, friendsOnly)
+      .then(setEntries)
+      .finally(() => {
+        setAreEntriesLoading(false);
+      });
+  }, [isLeaderboardLoading, getEntries, start, count, friendsOnly]);
+
+  const isLoading = isLeaderboardLoading || areEntriesLoading;
+
+  return {
+    isLoading,
+    entries,
+  };
+}
+
+export function useLeaderboardCurrentUserEntries(
+  leaderboardName: string,
+  leaderboardOptions?: LeaderboardOptions,
+): { isLoading: boolean; entries: LeaderboardEntries } {
+  const { isLoading: isLeaderboardLoading, getCurrentUserEntries } =
+    useLeaderboard(leaderboardName, leaderboardOptions);
+  const [entries, setEntries] = useState<LeaderboardEntries>([]);
+  const [areEntriesLoading, setAreEntriesLoading] = useState(true);
+
+  useEffect(() => {
+    if (isLeaderboardLoading) {
+      return;
+    }
+
+    setAreEntriesLoading(true);
+    getCurrentUserEntries()
+      .then(setEntries)
+      .finally(() => {
+        setAreEntriesLoading(false);
+      });
+  }, [isLeaderboardLoading, getCurrentUserEntries]);
+
+  const isLoading = isLeaderboardLoading || areEntriesLoading;
+
+  return {
+    isLoading,
+    entries,
+  };
+}
+
+export function useLeaderboardEntriesAroundCurrentUser(
+  leaderboardName: string,
+  leaderboardEntriesOptions: LeaderboardAroundCurrentUserEntriesOptions,
+  leaderboardOptions?: LeaderboardOptions,
+): { isLoading: boolean; entries: LeaderboardEntries } {
+  const { isLoading: isLeaderboardLoading, getEntriesAroundCurrentUser } =
+    useLeaderboard(leaderboardName, leaderboardOptions);
+  const [entries, setEntries] = useState<LeaderboardEntries>([]);
+  const [areEntriesLoading, setAreEntriesLoading] = useState(true);
+
+  const { countAhead, countBehind, friendsOnly } = leaderboardEntriesOptions;
+
+  useEffect(() => {
+    if (isLeaderboardLoading) {
+      return;
+    }
+
+    setAreEntriesLoading(true);
+    getEntriesAroundCurrentUser(countAhead, countBehind, friendsOnly)
+      .then(setEntries)
+      .finally(() => {
+        setAreEntriesLoading(false);
+      });
+  }, [
+    isLeaderboardLoading,
+    getEntriesAroundCurrentUser,
+    countAhead,
+    countBehind,
+    friendsOnly,
+  ]);
+
+  const isLoading = isLeaderboardLoading || areEntriesLoading;
+
+  return {
+    isLoading,
+    entries,
+  };
+}
